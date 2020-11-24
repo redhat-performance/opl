dd30581795d776cc9daa7e40fea7d43e79425dc0
Jan Hutar <jhutar@redhat.com>
2020-11-24 13:14:39 +0100

    tests/test_skelet.py: Fix skelet test

--- tests/test_skelet.py
+++ tests/test_skelet.py
@@ -10,6 +10,6 @@ class TestSkelet(unittest.TestCase):
 
     def test_test_setup(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument('fake_arg', help="Just to catch test file name when calling tests")
+        parser.add_argument('--fake_arg', help="Just to catch test file name when calling tests")
         with opl.skelet.test_setup(parser) as (args, status_data):
             self.assertIn('debug', args)
