90cb598aae329288f80bff54940f3b681d27b098
Jan Hutar <jhutar@redhat.com>
2021-03-04 13:13:31 +0100

    opl/pass_or_fail.py: Also show overall result

--- opl/pass_or_fail.py
+++ opl/pass_or_fail.py
@@ -12,6 +12,13 @@ import opl.investigator.status_data_loader
 import tabulate
 
 
+STATSUSES = {
+    0: 'PASS',
+    1: 'FAIL',
+    2: 'ERROR',
+}
+
+
 def main():
     parser = argparse.ArgumentParser(
         description='Given historical numerical data, determine if latest result is PASS or FAIL',
@@ -67,6 +74,7 @@ def main():
 
     print("\n", tabulate.tabulate(info_all, headers="keys", tablefmt="simple", floatfmt=".3f"))
     print("\n", tabulate.tabulate(summary, headers="keys", tablefmt="simple"))
+    print("\nOverall status: {STATSUSES[exit_code]}")
 
     if args.decisions_type == 'elasticsearch':
         opl.investigator.elasticsearch_decisions.store(args.decisions_es_server, args.decisions_es_index, info_all)
