ca7c0d44c0f5634fe4bee6a2b682355f417d0879
Jan Hutar <jhutar@redhat.com>
2021-03-04 15:00:03 +0100

    opl/status_data_updater.py: Add code to sync result from RP to ES

--- opl/status_data_updater.py
+++ opl/status_data_updater.py
@@ -15,9 +15,7 @@ import requests
 import tabulate
 
 
-def doit_list(args):
-    assert args.list_name is not None
-
+def _es_get_test(args, key, val, size=1):
     url = f"{args.es_server}/{args.es_index}/_search"
     headers = {
         'Content-Type': 'application/json',
@@ -28,7 +26,7 @@ def doit_list(args):
                 "filter": [
                     {
                         "term": {
-                            "name.keyword": args.list_name,
+                            key: val,
                         },
                     },
                 ],
@@ -39,14 +37,21 @@ def doit_list(args):
                 "order": "desc",
             },
         },
-        "size": args.list_size,
+        "size": size,
     }
 
     logging.info(f"Querying ES with url={url}, headers={headers} and json={json.dumps(data)}")
-
     response = requests.get(url, headers=headers, json=data)
     response.raise_for_status()
     logging.debug(f"Got back this: {json.dumps(response.json(), sort_keys=True, indent=4)}")
+
+    return response.json()
+
+
+def doit_list(args):
+    assert args.list_name is not None
+
+    response = _es_get_test(args, "name.keyword", args.list_name, args.list_size)
 
     table_headers = [
         'Run ID',
@@ -57,7 +62,7 @@ def doit_list(args):
     ]
     table = []
 
-    for item in response.json()['hits']['hits']:
+    for item in response['hits']['hits']:
         logging.debug(f"Loading data from document ID {item['_id']} with field id={item['_source']['id'] if 'id' in item['_source'] else None}")
         tmpfile = tempfile.NamedTemporaryFile(prefix=item['_id'], delete=False).name
         sd = opl.status_data.StatusData(tmpfile, data=item['_source'])
@@ -75,37 +80,9 @@ def doit_list(args):
 def doit_change(args):
     assert args.change_id is not None
 
-    url = f"{args.es_server}/{args.es_index}/_search"
-    headers = {
-        'Content-Type': 'application/json',
-    }
-    data = {
-        "query": {
-            "bool": {
-                "filter": [
-                    {
-                        "term": {
-                            "id.keyword": args.change_id,
-                        },
-                    },
-                ],
-            },
-        },
-        "sort": {
-            "started": {
-                "order": "desc",
-            },
-        },
-        "size": 1,
-    }
+    response = _es_get_test(args, "id.keyword", args.change_id)
 
-    logging.info(f"Querying ES with url={url}, headers={headers} and json={json.dumps(data)}")
-
-    response = requests.get(url, headers=headers, json=data)
-    response.raise_for_status()
-    logging.debug(f"Got back this: {json.dumps(response.json(), sort_keys=True, indent=4)}")
-
-    source = response.json()['hits']['hits'][0]
+    source = response['hits']['hits'][0]
     es_type = source['_type']
     es_id = source['_id']
     logging.debug(f"Loading data from document ID {source['_id']} with field id={source['_source']['id']}")
@@ -154,29 +131,125 @@ def doit_change(args):
     print(sd.info())
 
 
+def doit_rp_to_es(args):
+    assert args.es_server is not None
+    assert args.rp_host is not None
+
+    RP_TO_ES_STATE = {
+        "automation_bug": "FAIL",
+        "no_defect": "PASS",
+        "product_bug": "FAIL",
+        "system_issue": "ERROR",
+        "to_investigate": "FAIL",
+    }
+
+    # Start a session
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {args.rp_token}',
+    }
+    session = requests.Session()
+
+    # Get 10 newest launches
+    url = f'https://{args.rp_host}/api/v1/{args.rp_project}/launch'
+    data = {
+      "filter.eq.name": args.rp_launch,
+      "page.size": 3,
+      "page.sort": "endTime,desc",
+    }
+    logging.debug(f"Going to do GET request to {url} with {data}")
+    response = session.get(url, params=data, headers=headers, verify=not args.rp_noverify)
+    if not response.ok:
+        logging.error(f"Request failed: {response.text}")
+    response.raise_for_status()
+    logging.debug(f"Request returned {response.json()}")
+    launches = response.json()['content']
+
+    for launch in launches:
+        # Get run ID from launch attributes
+        run_id = None
+        for a in launch["attributes"]:
+            if a["key"] == "run_id":
+                run_id = a["value"]
+                break
+        assert run_id is not None
+
+        # Get result from launch statistics
+        assert sum([d["total"] for d in launch["statistics"]["defects"].values()]) == 1
+        assert len(list(launch["statistics"]["defects"].keys())) == 1
+        result = RP_TO_ES_STATE[list(launch["statistics"]["defects"].keys())[0]]
+
+        # Get tests in the launch
+        response = _es_get_test(args, "id.keyword", run_id)
+        source = response['hits']['hits'][0]
+        es_type = source['_type']
+        es_id = source['_id']
+        logging.debug(f"Loading data from document ID {source['_id']} with field id={source['_source']['id']}")
+        tmpfile = tempfile.NamedTemporaryFile(prefix=source['_id'], delete=False).name
+        sd = opl.status_data.StatusData(tmpfile, data=source['_source'])
+
+        if sd.get("result") != result:
+            logging.info(f"Results do not match, updating them: {sd.get('result')} != {result}")
+            sd.set("result", result)
+
+            # Add comment to log the change
+            if sd.get('comments') is None:
+                sd.set('comments', [])
+            if not isinstance(sd.get('comments'), list):
+                logging.error(f"Field 'comments' is not a list: {sd.get('comments')}")
+                raise Exception(f"Field 'comments' is not a list: {sd.get('comments')}")
+            sd.get('comments').append({
+                'author': "status_data_updater",
+                'date': datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat(),
+                'text': "Automatic update as per ReportPortal change",
+            })
+
+            # Save the changes to ES
+            url = f"{args.es_server}/{args.es_index}/{es_type}/{es_id}"
+            logging.info(f"Saving to ES with url={url} and json={json.dumps(sd.dump())}")
+            response = requests.post(url, json=sd.dump())
+            response.raise_for_status()
+            logging.debug(f"Got back this: {json.dumps(response.json(), sort_keys=True, indent=4)}")
+
+
 def main():
     parser = argparse.ArgumentParser(
         description='Investigate and modify status data documents in ElasticSearch',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument('--action', required=True,
-                        choices=['list', 'change'],
+                        choices=['list', 'change', 'rp-to-es'],
                         help='What action to do')
+
     parser.add_argument('--es-server',
                         default='http://elasticsearch.example.com:9286',
                         help='ElasticSearch server for the results data')
     parser.add_argument('--es-index', default='my-index',
                         help='ElasticSearch index for the results data')
+
     parser.add_argument('--list-name',
                         help='Name of the test to query for when listing')
     parser.add_argument('--list-size', type=int, default=50,
                         help='Number of documents to show when listing')
+
     parser.add_argument('--change-id',
                         help='ID of a test run when changing')
     parser.add_argument('--change-set', nargs='*', default=[],
                         help='Set key=value data')
     parser.add_argument('--change-comment-text',
                         help='Comment to be added as part of change')
+
+    parser.add_argument('--rp-host',
+                        help='ReportPortal host')
+    parser.add_argument('--rp-noverify', action='store_true',
+                        help='When talking to ReportPortal ignore certificate verification failures')
+    parser.add_argument('--rp-project',
+                        help='ReportPortal project')
+    parser.add_argument('--rp-token',
+                        help='ReportPortal token')
+    parser.add_argument('--rp-launch',
+                        help='ReportPortal launch name')
+
     parser.add_argument('-d', '--debug', action='store_true',
                         help='Show debug output')
     args = parser.parse_args()
@@ -190,3 +263,5 @@ def main():
         return doit_list(args)
     if args.action == 'change':
         return doit_change(args)
+    if args.action == 'rp-to-es':
+        return doit_rp_to_es(args)
