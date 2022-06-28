defda09bdd5fcb965d8f470c286b597544b55c9a
Jan Hutar <jhutar@redhat.com>
2022-06-28 10:17:03 +0200

    fix: Fixing tests to match inventory_egress.py

--- tests/test_generators_insights_egress.py
+++ tests/test_generators_insights_egress.py
@@ -14,20 +14,20 @@ class TestPayloadGenerator(unittest.TestCase):
 
     def test_counting(self):
         pg = opl.generators.inventory_egress.EgressHostsGenerator(
-            expected=1,
+            count=3,
             n_packages=10,
             msg_type='created')
-        self.assertEqual(pg.expected, 1)
-        self.assertEqual(pg.generated, 0)
+        self.assertEqual(pg.count, 3)
         self.assertEqual(pg.n_packages, 10)
         self.assertEqual(pg.msg_type, 'created')
 
     def test_count(self):
-        pg = opl.generators.inventory_egress.EgressHostsGenerator(
-            expected=1,
-            n_packages=10,
-            msg_type='created')
+        pg = opl.generators.inventory_egress.EgressHostsGenerator()
         mid, msg = next(pg)
-        self.assertIsInstance(msg, str)
         with self.assertRaises(StopIteration):
             mid, msg = next(pg)
+
+    def test_type(self):
+        pg = opl.generators.inventory_egress.EgressHostsGenerator()
+        mid, msg = next(pg)
+        self.assertIsInstance(msg, dict)
