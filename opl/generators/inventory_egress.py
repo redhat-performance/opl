8f21b51e0b905d2e0db188c128aaf232469a3caa
Jan Hutar <jhutar@redhat.com>
2022-08-24 10:02:32 +0200

    feat: Also allow setting machine_id in the template

--- opl/generators/inventory_egress.py
+++ opl/generators/inventory_egress.py
@@ -44,6 +44,7 @@ class EgressHostsGenerator(opl.generators.generic.GenericGenerator):
                 + random.sample(self.data['AVAILABLE_REPOS'], 10),   # noqa: W503
             'b64_identity': self._get_b64_identity(account),
             'msg_type': self.msg_type,
+            'machine_id': self._get_rhel_machine_id(),
             'subscription_manager_id': self._get_uuid(),
             'bios_uuid': self._get_bios_uuid(),
             'ipv4_addr': self._get_ipv4(),
