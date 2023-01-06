7f0d6cecd5fb29623c19b512ec3d28eed5c20a44
Jan Hutar <jhutar@redhat.com>
2023-01-06 11:14:29 +0100

    fix(opl/investigator/elasticsearch_loader.py): This way when request fails, we also get it's response.text in debug message

--- opl/investigator/elasticsearch_loader.py
+++ opl/investigator/elasticsearch_loader.py
@@ -2,9 +2,8 @@ import json
 import logging
 import tempfile
 
+import opl.http
 import opl.status_data
-
-import requests
 
 
 def load(server, index, query, paths):
@@ -20,11 +19,9 @@ def load(server, index, query, paths):
     data = query
     logging.info(f"Querying ES with url={url}, headers={headers} and json={json.dumps(data)}")
 
-    response = requests.get(url, headers=headers, json=data)
-    response.raise_for_status()
-    # logging.debug(f"Got back this: {json.dumps(response.json(), sort_keys=True, indent=4)}")
+    response = opl.http.get(url, headers=headers, json=data)
 
-    for item in response.json()['hits']['hits']:
+    for item in response['hits']['hits']:
         logging.debug(f"Loading data from document ID {item['_id']} with field id={item['_source']['id'] if 'id' in item['_source'] else None} or parameters.run={item['_source']['parameters']['run'] if 'run' in item['_source']['parameters'] else None}")
         tmpfile = tempfile.NamedTemporaryFile(prefix=item['_id'], delete=False).name
         sd = opl.status_data.StatusData(tmpfile, data=item['_source'])
