dc605ac597bfa88cbdbbcc2877daf88dfa8889ac
Rajaditya Chauhan <rajchauh@redhat.com>
2023-03-13 17:32:20 +0530

    opl: generators are first subjected to black formatting followed by falke8 necessary changes

--- opl/generators/packages.py
+++ opl/generators/packages.py
@@ -9,10 +9,10 @@ import random
 class PackagesGenerator:
     def __init__(self):
         data_dirname = os.path.dirname(__file__)
-        self.data_file = os.path.join(data_dirname, 'packages_data.json')
+        self.data_file = os.path.join(data_dirname, "packages_data.json")
 
         # Load data
-        with open(self.data_file, 'r') as fp:
+        with open(self.data_file, "r") as fp:
             data_raw = json.load(fp)
 
         # Only pick one version and drop rest of them to make
@@ -39,11 +39,16 @@ class PackagesGenerator:
 class YumReposGenerator:
     def __init__(self):
         data_dirname = os.path.dirname(__file__)
-        self.data = [yum_repo for yum_repo in open(os.path.join(data_dirname, 'yum_repos.txt'), 'r').read().split('\n')]
-    
+        self.data = [
+            yum_repo
+            for yum_repo in open(os.path.join(data_dirname, "yum_repos.txt"), "r")
+            .read()
+            .split("\n")
+        ]
+
     def count(self):
         return len(self.data)
-    
+
     def generate(self, count):
         return random.sample(self.data, count)
 
@@ -51,11 +56,16 @@ class YumReposGenerator:
 class EnabledServicesGenerator:
     def __init__(self) -> None:
         data_dirname = os.path.dirname(__file__)
-        self.data = [service for service in open(os.path.join(data_dirname, 'enabled_services.txt'), 'r').read().split('\n')]
-    
+        self.data = [
+            service
+            for service in open(os.path.join(data_dirname, "enabled_services.txt"), "r")
+            .read()
+            .split("\n")
+        ]
+
     def count(self):
         return len(self.data)
-    
+
     def generate(self, count):
         return random.sample(self.data, count)
 
@@ -63,11 +73,18 @@ class EnabledServicesGenerator:
 class InstalledServicesGenerator:
     def __init__(self) -> None:
         data_dirname = os.path.dirname(__file__)
-        self.data = [service for service in open(os.path.join(data_dirname, 'installed_services.txt'), 'r').read().split('\n')]
-    
+        self.data = [
+            service
+            for service in open(
+                os.path.join(data_dirname, "installed_services.txt"), "r"
+            )
+            .read()
+            .split("\n")
+        ]
+
     def count(self):
         return len(self.data)
-    
+
     def generate(self, count):
         return random.sample(self.data, count)
 
@@ -75,10 +92,17 @@ class InstalledServicesGenerator:
 class RunningProcessesGenerator:
     def __init__(self) -> None:
         data_dirname = os.path.dirname(__file__)
-        self.data = [service for service in open(os.path.join(data_dirname, 'running_processes.txt'), 'r').read().split('\n')]
-    
+        self.data = [
+            service
+            for service in open(
+                os.path.join(data_dirname, "running_processes.txt"), "r"
+            )
+            .read()
+            .split("\n")
+        ]
+
     def count(self):
         return len(self.data)
-    
+
     def generate(self, count):
         return random.sample(self.data, count)
