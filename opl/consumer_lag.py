cc27f4cead5d60f6a67b90f7a681d76282056e17
Rajaditya Chauhan <rajchauh@redhat.com>
2023-03-20 14:41:46 +0530

    consumer_lag.py : black and flake8 compatible

--- opl/consumer_lag.py
+++ opl/consumer_lag.py
@@ -3,7 +3,6 @@
 import logging
 import time
 from kafka import KafkaConsumer
-from kafka.consumer import group
 from kafka import TopicPartition
 
 
@@ -12,13 +11,13 @@ class ConsumerLag:
     ConsumerLag gets the lag info for a particular topic of all the partitions by providing
     bootstrap_server and kafka group as input.
     """
+
     def __init__(self, topic, bootstrap_servers, group) -> None:
         self.topic = topic
         self.group = group
         self.bootstrap_servers = bootstrap_servers
-        self.logger = logging.getLogger('consumer_lag')
+        self.logger = logging.getLogger("consumer_lag")
         self.offset_records = {}
-    
 
     def _getconsumer(self):
         consumer = KafkaConsumer(
@@ -30,10 +29,10 @@ class ConsumerLag:
             group_id=self.group,
             session_timeout_ms=50000,
             heartbeat_interval_ms=10000,
-            consumer_timeout_ms=100000)
-        
-        return consumer
+            consumer_timeout_ms=100000,
+        )
 
+        return consumer
 
     def store_offset_records(self):
         consumer = self._getconsumer()
@@ -57,23 +56,29 @@ class ConsumerLag:
             curr_offsets[partition.partition] = committed
 
         end_offsets = consumer.end_offsets(partitions)
-    
-        for partition_id, value in curr_offsets.items():
-            record = {'curr_offset': value, 'end_offset': end_offsets[TopicPartition(topic=self.topic, partition=partition_id)]}
-            self.offset_records[partition_id] = record
 
+        for partition_id, value in curr_offsets.items():
+            record = {
+                "curr_offset": value,
+                "end_offset": end_offsets[
+                    TopicPartition(topic=self.topic, partition=partition_id)
+                ],
+            }
+            self.offset_records[partition_id] = record
 
     def get_lag(self):
         self.store_offset_records()
         response = True
-        for _ , record in self.offset_records.items():
-            if record['curr_offset'] is None:
-                self.logger.warning(f"For some reason current offset is None, replacing it with 0 in: {record}")
-                record['curr_offset'] = 0
-            diff = record['end_offset'] - record['curr_offset']
+        for _, record in self.offset_records.items():
+            if record["curr_offset"] is None:
+                self.logger.warning(
+                    f"For some reason current offset is None, replacing it with 0 in: {record}"
+                )
+                record["curr_offset"] = 0
+            diff = record["end_offset"] - record["curr_offset"]
             if diff == 0:
                 response *= True
             else:
                 response *= False
-        
+
         return response
