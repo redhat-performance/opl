793e15cf225c1a4ffd167746638da121ccbbd1a1
Rajaditya Chauhan <rajchauh@redhat.com>
2023-03-20 15:53:33 +0530

    elasticsearch_decisions.py : black and flake8 compatible

--- opl/investigator/elasticsearch_decisions.py
+++ opl/investigator/elasticsearch_decisions.py
@@ -7,23 +7,24 @@ import requests
 
 
 def store(server, index, decisions):
-
     # This is our workaround on how to add additional metadata about the decision
-    job_name = os.environ.get('JOB_NAME', '')
-    build_url = os.environ.get('BUILD_URL', '')
+    job_name = os.environ.get("JOB_NAME", "")
+    build_url = os.environ.get("BUILD_URL", "")
 
     url = f"{server}/{index}/_doc"
     headers = {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
     }
     for decision in decisions:
-        decision['job_name'] = job_name
-        decision['build_url'] = build_url
-        decision['uploaded'] = datetime.datetime.utcnow().isoformat()
+        decision["job_name"] = job_name
+        decision["build_url"] = build_url
+        decision["uploaded"] = datetime.datetime.utcnow().isoformat()
 
-        ### for k, v in decision.items():
-        ###    print(f">>> {k} = {v} ({type(v)})")
-        logging.info(f"Storing decision to ES url={url}, headers={headers} and json={json.dumps(decision)}")
+        # for k, v in decision.items():
+        #    print(f">>> {k} = {v} ({type(v)})")
+        logging.info(
+            f"Storing decision to ES url={url}, headers={headers} and json={json.dumps(decision)}"
+        )
 
         response = requests.post(url, headers=headers, json=decision)
 
