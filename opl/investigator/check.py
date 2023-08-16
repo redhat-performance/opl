00e204991605a5c2d1105f6d0eec0297702422ac
Elijah DeLee <kdelee@redhat.com>
2023-08-16 13:59:12 -0400

    fix lint

--- opl/investigator/check.py
+++ opl/investigator/check.py
@@ -41,15 +41,16 @@ def _check_by_min_max(data, value):
     )
     return lower_boundary <= value <= upper_boundary, info
 
+
 def _check_by_stdev(data, value, num_deviations):
     logging.debug(f"data={data} and value={value}")
     mean = statistics.mean(data)
     stdev = statistics.stdev(data)
-    acceptable_deviation = stdev*num_deviations
+    acceptable_deviation = stdev * num_deviations
     lower_boundary = float(mean - acceptable_deviation)
     upper_boundary = float(mean + acceptable_deviation)
     logging.info(
-            f"value={value}, data len={len(data)} mean={mean:.03f}, stdev={stdev:.03f}, boundaries={lower_boundary:.03f}--{upper_boundary:.03f}"
+        f"value={value}, data len={len(data)} mean={mean:.03f}, stdev={stdev:.03f}, boundaries={lower_boundary:.03f}--{upper_boundary:.03f}"
     )
     info = collections.OrderedDict(
         [
@@ -66,6 +67,7 @@ def _check_by_stdev(data, value, num_deviations):
     )
     return lower_boundary <= value <= upper_boundary, info
 
+
 def check_by_iqr(data, value):
     """Checks if the current value is within the interquartile range of the previous values"""
     logging.debug(f"data={data} and value={value}")
@@ -74,7 +76,7 @@ def check_by_iqr(data, value):
     lower_boundary = float(quantiles[0])
     upper_boundary = float(quantiles[2])
     logging.info(
-            f"value={value}, data len={len(data)} mean={mean:.03f}, boundaries={lower_boundary:.03f}--{upper_boundary:.03f}"
+        f"value={value}, data len={len(data)} mean={mean:.03f}, boundaries={lower_boundary:.03f}--{upper_boundary:.03f}"
     )
     info = collections.OrderedDict(
         [
@@ -93,17 +95,19 @@ def check_by_iqr(data, value):
 
 
 def check_by_min_max_0_1(data, value):
-    """Checks if the current value is within the min/max range of previous values
-    """
+    """Checks if the current value is within the min/max range of previous values"""
     return _check_by_min_max(data, value)
+
 
 def check_by_stdev_1(data, value):
     """Checks if the current value is within 1 standard deviations of the mean of previous values"""
     return _check_by_stdev(data, value, 1)
 
+
 def check_by_stdev_2(data, value):
     """Checks if the current value is within 2 standard deviations of the mean of previous values"""
     return _check_by_stdev(data, value, 2)
+
 
 def check_by_stdev_3(data, value):
     """Checks if the current value is within 2 standard deviations of the mean of previous values"""
