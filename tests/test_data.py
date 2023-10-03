cb7af1e1d862a1982e1d5de5482a7fb84fbb0538
Pablo Méndez Hernández <pablomh@redhat.com>
2023-10-03 12:57:17 +0200

    feat(tests/test_data.py): Add histogram test

--- tests/test_data.py
+++ tests/test_data.py
@@ -42,6 +42,24 @@ class TestSkelet(unittest.TestCase):
         self.assertEqual(stats['min'], datetime.datetime.fromisoformat('2021-03-22T11:00:00.000000+00:00'))
         self.assertEqual(stats['range'].total_seconds(), 3600)
 
+    def test_get_hist(self):
+        hist = opl.data.get_hist([0, 1, 1, 2, 2, 1, 1, 0])
+        self.assertEqual(
+            hist,
+            [
+                ((0.0, 0.2), 2.0),
+                ((0.2, 0.4), 0.0),
+                ((0.4, 0.6000000000000001), 0.0),
+                ((0.6000000000000001, 0.8), 0.0),
+                ((0.8, 1.0), 0.0),
+                ((1.0, 1.2000000000000002), 4.0),
+                ((1.2000000000000002, 1.4000000000000001), 0.0),
+                ((1.4000000000000001, 1.6), 0.0),
+                ((1.6, 1.8), 0.0),
+                ((1.8, 2.0), 2.0),
+            ],
+        )
+
     def test_get_rps(self):
         rps_vals = opl.data.get_rps([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], bucket_size=10, granularity=1)
         self.assertEqual(len(rps_vals), 10)
