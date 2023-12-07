2157799a7a9f17cf6404381de4ee3a0fd2f3ce9a
Jan Smejkal <jsmejkal@redhat.com>
2023-12-07 22:45:36 +0100

    fix(HBI_populate): Add support for multiple brokers in arg

--- opl/hbi_utils.py
+++ opl/hbi_utils.py
@@ -190,7 +190,7 @@ def gen_send_verify(args, status_data):
     else:
         status_data.set("parameters.kafka.bootstrap", kafka_host)
         producer = kafka.KafkaProducer(
-            bootstrap_servers=[kafka_host], api_version=(0, 10)
+            bootstrap_servers=kafka_host, api_version=(0, 10)
         )
 
     logging.info("Creating data structure to store list of accounts and so")
