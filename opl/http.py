2739348771ca3c3d8bf900e53eef25198f681594
spadakan <spadakan@redhat.com>
2023-12-18 07:27:10 -0500

    fixing black formatter issues

--- opl/http.py
+++ opl/http.py
@@ -2,20 +2,23 @@ import logging
 
 import requests
 from requests.auth import HTTPBasicAuth
+
 import urllib3
 import os
 
-enable_new_search = os.getenv('NEW_SEARCH_ENABLED', 'false').lower() == 'true'
+enable_new_search = os.getenv("NEW_SEARCH_ENABLED", "false").lower() == "true"
 
 if enable_new_search:
-    username = os.getenv('OPEN_SEARCH_DASBOARD')
-    password = os.getenv('OPEN_SEARCH_PASSWORD')
+    username = os.getenv("OPEN_SEARCH_USERNAME")
+    password = os.getenv("OPEN_SEARCH_PASSWORD")
     session = requests.Session()
     session.auth = HTTPBasicAuth(username, password)
     session.verify = False
-    session.headers.update({
-        "Content-Type": "application/json",
-    })
+    session.headers.update(
+        {
+            "Content-Type": "application/json",
+        }
+    )
 else:
     session = requests.Session()
 
