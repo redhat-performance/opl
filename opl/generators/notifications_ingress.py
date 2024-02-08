20da717f175299121c93c8cbb01582a0bf3cfef8
Larry Rios <lrios@redhat.com>
2024-02-08 01:48:07 -0500

    adding bundles and event_type

--- opl/generators/notifications_ingress.py
+++ opl/generators/notifications_ingress.py
@@ -4,13 +4,26 @@ import opl.generators.generic
 
 
 class NotificationsMessagesGenerator(opl.generators.generic.GenericGenerator):
-    def __init__(self, count=1, template="notifications_ingres_template.json.j2"):
+    def __init__(
+        self,
+        count=1,
+        template="notifications_ingres_template.json.j2",
+        event_type="integration-failed",
+        bundle="console",
+        application="integrations",
+    ):
+        self.event_type = event_type
+        self.bundle = bundle
+        self.application = application
         super().__init__(count=count, template=template, dump_message=False)
 
     def _data(self):
         account = self._get_account()
 
         return {
+            "application": self.application,
+            "bundle": self.bundle,
+            "event_type": self.event_type,
             "notifications_id": self._get_uuid(),
             "account": account,
             "fqdn": self._get_hostname(),
