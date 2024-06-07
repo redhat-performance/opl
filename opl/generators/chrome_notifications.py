4abaf9a0a4852ababcbfe5016d2bb12e9fb5e3d9
Jan Smejkal <jsmejkal@redhat.com>
2024-06-07 17:08:49 +0200

    fix(chrome gen): Fix chrome generator method names and add missing _mid() method to send messages

--- opl/generators/chrome_notifications.py
+++ opl/generators/chrome_notifications.py
@@ -2,10 +2,6 @@
 # sample message:
 # echo '{"specversion":"1.0.2","type":"notifications.drawer","source":"https://whatever.service.com","id":"test-message","time":"2023-05-23T11:54:03.879689005+02:00","datacontenttype":"application/json","data":{"broadcast":true,"payload":{"id":"foo.bar.1","description":"string","title":"string","created":"2023-05-23T11:54:03.879689005+02:00","read":false,"source":"string"}}}'
 
-import json
-import os.path
-import random
-
 import opl.gen
 import opl.generators.packages
 import opl.generators.generic
@@ -19,16 +15,13 @@ class ChromeNotificationsGenerator(opl.generators.generic.GenericGenerator):
     ):
         super().__init__(count=count, template=template, dump_message=False)
 
-        # Load package profile generator
-        self.pg = opl.generators.packages.PackagesGenerator(self.package_file)
-
-    # def _mid(self, data):
-    #     return data["inventory_id"]
+    def _mid(self, data):
+        return data["id"]
 
     def _data(self):
         return {
-            "id": self.gen_uuid(),
-            "payload_id": self.gen_uuid(),
-            "time": self.gen_datetime(),
-            "title": self.gen_string(),
+            "id": self._get_uuid(),
+            "payload_id": self._get_uuid(),
+            "time": self._get_now_iso(),
+            "title": "totallyrandomtitle",
         }
