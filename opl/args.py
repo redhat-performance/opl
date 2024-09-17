f54748ce5050ca84b0243cc05cde2f152a436fe3
Rajaditya Chauhan <rajchauh@redhat.com>
2024-09-17 18:59:46 +0530

    Export: adding export db args in args.py

--- opl/args.py
+++ opl/args.py
@@ -86,6 +86,10 @@ def add_ros_db_opts(parser):
     _add_generic_db_opts(parser, "ros")
 
 
+def add_export_db_opts(parser):
+    _add_generic_db_opts(parser, "export")
+
+
 def add_kafka_opts(parser):
     parser.add_argument(
         "--kafka-host",
